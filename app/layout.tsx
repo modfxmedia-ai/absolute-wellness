8ff9b0327eee923c26759b9c5c9ce0355d98cfa2
nargis-ai <nargis-ai@users.noreply.github.com>
2026-07-01 02:45:26 +0500

    ga code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Raleway, Lato } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -66,6 +67,7 @@ export default function RootLayout({
         <main className="flex-1">{children}</main>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-2Q667CTX7D" />
     </html>
   );
 }
